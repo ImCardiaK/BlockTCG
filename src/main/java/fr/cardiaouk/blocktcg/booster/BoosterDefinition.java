d197a4d0897fc5c8db8c040c304c60faf579e807
Cédric C. <85906923+ImCardiaK@users.noreply.github.com>
2026-08-18 00:15:42 +0200

    Delete src/main directory

--- src/main/java/fr/cardiaouk/blocktcg/booster/BoosterDefinition.java
+++
@@ -1,6 +0,0 @@
-package fr.cardiaouk.blocktcg.booster;
-
-import org.bukkit.Material;
-import java.util.Map;
-
-public record BoosterDefinition(String id, String name, Material material, int price, int cards, Map<String,Integer> rarityWeights, String theme) {}
--- src/main/java/fr/cardiaouk/blocktcg/prebuilt/PrebuiltDeck.java
+++
@@ -1,5 +0,0 @@
-package fr.cardiaouk.blocktcg.prebuilt;
-
-import java.util.List;
-
-public record PrebuiltDeck(String id, String name, String theme, int price, String container, List<String> cards) {}
